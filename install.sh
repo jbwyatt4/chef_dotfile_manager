07e6c53858300285f3bc7684885bf8abc400dd5f
jbwyatt4 <jbwyatt4@gmail.com>
2018-04-29 23:00:00 -0700

    Fix node files being set to root

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ echo 'Run ./run.sh after this'
 sudo chef-solo -c solo.rb -o solobook::root_only
 # after a run the cache folder is set as root and needs to be deleted
 sudo rm -rf local-mode-cache
+sudo rm -rf nodes/*.json # if you run the install first the node files will be set to root and chef will complain
